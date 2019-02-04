db6e8312d31e10e29b98e93dd50baed085a36a20
Mac <mac@Mac-MacBook-Pro.local>
2019-02-04 14:22:08 +0300

    deploy

---
+++ build/precache-manifest.00bde9d97f9801e0662cd585915495ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c2db4a0698668271827",
+    "url": "/static/js/main.5c2db4a0.chunk.js"
+  },
+  {
+    "revision": "e4ed8f5f2f933d53b325",
+    "url": "/static/js/1.e4ed8f5f.chunk.js"
+  },
+  {
+    "revision": "5c2db4a0698668271827",
+    "url": "/static/css/main.7e38182f.chunk.css"
+  },
+  {
+    "revision": "0ebe62370f892c18844cb0dd294f4c02",
+    "url": "/index.html"
+  }
+];
